3d0defef54b0023868971938f0e27785861c0e37
jayseaCODE <jayseaLEARN@hotmail.com>
2018-11-07 14:12:36 +1300

    feat: script for obstacle to indicate character gains a point in passing it

---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour {
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.GetComponent<Bird>() != null)
+        {
+            //If the character hits the trigger collider in between the obstacles then
+            //tell the game control that the character scored.
+            GameControl.instance.CharacterScored();
+        }
+    }
+}
