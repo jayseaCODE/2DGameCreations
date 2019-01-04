86ae6835c624db29938234fc992243f6d1e95a0f
jayseaCODE <jayseaLEARN@hotmail.com>
2019-01-04 13:33:31 +1300

    feat: To detect when a player has successfully passed a set of columns

---
+++ Assets/Column.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Column : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.GetComponent<Drifting>() != null)
+        {
+            //If Poogle hits the trigger collider in between the water columns then
+            //tell the game control that Poogle scored.
+            GameControl.instance.CharacterScored();
+        }
+    }
+}
