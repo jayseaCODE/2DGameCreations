612a12b75cf29af060e7e576f0ec6a52a23dab23
jayseaCODE <jayseaLEARN@hotmail.com>
2019-03-20 18:28:29 +1300

    feat: basic pause menu with more work to it to do

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour {
+
+    public GameObject pauseMenuUI;
+    public static bool GameIsPaused = false;
+
+    public void Start()
+    {
+        if (pauseMenuUI == null)
+        {
+            Debug.LogError("Pause Menu Script has missing PauseMenuUI reference in Canvas!");
+            UnityEditor.EditorApplication.isPlaying = false;
+        }
+    }
+
+
+    public void ResumeGame()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    public void PauseGame()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        // SceneManager.LoadScene...
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
