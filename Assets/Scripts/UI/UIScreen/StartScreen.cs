5397ed20b6ace18656f70a9beef8af83ba1a2f0b
jayseaCODE <jayseaLEARN@hotmail.com>
2019-04-19 13:58:46 +1200

    Refactor: Start Screen at the beginning of game

---
+++ Assets/Scripts/UI/UIScreen/StartScreen.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Poogle.UI
+{
+    public class StartScreen : UIScreen
+    {
+        protected Button PlayButton = null;
+        protected Button HelpButton = null;
+        protected Button InfoButton = null;
+        protected Button ExitButton = null;
+
+        void Start()
+        {
+            PlayButton.SetButtonAction(() =>
+            {
+                var uiManager = UIManager.Singleton;
+                var inGameScreen = uiManager.Screens.Find(screen => screen.ScreenInfo == UIScreenInfo.IN_GAME_SCREEN);
+                if (inGameScreen != null)
+                {
+                    uiManager.OpenScreen(inGameScreen);
+                    //GameManager.Singleton.StartGame();
+                }
+            });
+
+            ExitButton.SetButtonAction(() =>
+            {
+                //GameManager.Singleton.ExitGame();
+            });
+        }
+
+        public override void UpdateScreenStatus(bool open)
+        {
+            base.UpdateScreenStatus(open);
+        }
+    }
+}
