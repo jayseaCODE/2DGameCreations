8ae32748c047396c5aa3c60bc71b9cd55ba6b0ac
jayseaCODE <jayseaLEARN@hotmail.com>
2019-04-19 13:56:12 +1200

    Refactor: UIScreen to be the base of all Screens presented

---
+++ Assets/Scripts/UI/UIScreen/UIScreen.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Poogle.UI
+{
+    public class UIScreen : MonoBehaviour
+    {
+        internal UIScreenInfo ScreenInfo;
+
+        public bool IsOpen { get; set; }
+
+        public virtual void UpdateScreenStatus(bool open)
+        {
+            IsOpen = open;
+        }
+    }
+}
