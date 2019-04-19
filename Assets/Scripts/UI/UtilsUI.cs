fbed602c19f1c1201240d53efb6ba6426ec92329
jayseaCODE <jayseaLEARN@hotmail.com>
2019-04-19 13:58:08 +1200

    Refactor: UI utilities

---
+++ Assets/Scripts/UI/UtilsUI.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine.UI;
+
+namespace Poogle
+{
+    public static class UtilsUI
+    {
+        public static void SetButtonAction(this Button button, Action action)
+        {
+            button.onClick.RemoveAllListeners();
+            button.onClick.AddListener(() => action());
+        }
+    }
+}
