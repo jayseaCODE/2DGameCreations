17e0bf41ed5c31f970560d8f92a6954c11b70c66
jayseaCODE <jayseaLEARN@hotmail.com>
2019-04-28 17:53:04 +1200

    Refactor: Loading Screen script on Loading Canvasgroup

---
+++ Assets/Scripts/UI/UIScreen/LoadingScreen.cs
@@ -0,0 +1,15 @@
+﻿using Poogle.UI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Poogle
+{
+    public class LoadingScreen : UIScreen
+    {
+        public override void UpdateScreenStatus(bool open)
+        {
+            base.UpdateScreenStatus(open);
+        }
+    }
+}
