ea59b950a4ccddabc1b41e6b15134f6e533a60b8
jayseaCODE <jayseaLEARN@hotmail.com>
2019-04-28 16:40:46 +1200

    Feat: Update interactivity on screens in CanvasGroup

--- Assets/Scripts/UI/UIScreen/UIScreen.cs
+++ Assets/Scripts/UI/UIScreen/UIScreen.cs
@@ -6,10 +6,13 @@ namespace Poogle.UI
     {
         internal UIScreenInfo ScreenInfo;
 
+        protected CanvasGroup m_CanvasGroup;
         public bool IsOpen { get; set; }
 
         public virtual void UpdateScreenStatus(bool open)
         {
+            m_CanvasGroup.interactable = open;
+            m_CanvasGroup.blocksRaycasts = open;
             IsOpen = open;
         }
     }
