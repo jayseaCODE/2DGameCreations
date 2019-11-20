3d98438c59b275b65ab20da90fb8289aff5f0d83
Joshua Chen <jaysealearn@hotmail.com>
2019-11-20 13:27:40 +1300

    feat: button control over UI elements

---
+++ Assets/Scripts/UI/UIButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+namespace Poogle.UI
+{
+    public class UIButton : Button
+    {
+        public override void OnPointerDown(UnityEngine.EventSystems.PointerEventData eventData)
+        {
+            if (IsActive() && IsInteractable())
+            {
+                //AudioManager.Singleton.PlayClickSound();
+            }
+            base.OnPointerDown(eventData);
+        }
+    }
+}
+
