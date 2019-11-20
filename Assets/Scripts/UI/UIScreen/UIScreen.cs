345109d67375173496be59300e5d5341cb2f4c88
Joshua Chen <jaysealearn@hotmail.com>
2019-11-20 13:28:48 +1300

    refactor: UIScreen to serialize inputs from Editor, such as animator

--- Assets/Scripts/UI/UIScreen/UIScreen.cs
+++ Assets/Scripts/UI/UIScreen/UIScreen.cs
@@ -4,13 +4,18 @@ namespace Poogle.UI
 {
     public class UIScreen : MonoBehaviour
     {
+        [SerializeField]
         internal UIScreenInfo ScreenInfo;
-
+        [SerializeField]
+        protected Animator m_Animator;
+        [SerializeField]
         protected CanvasGroup m_CanvasGroup;
+
         public bool IsOpen { get; set; }
 
         public virtual void UpdateScreenStatus(bool open)
         {
+            m_Animator.SetBool("Open", open);
             m_CanvasGroup.interactable = open;
             m_CanvasGroup.blocksRaycasts = open;
             IsOpen = open;
